1dd44b3b32ad0f6a6c1dec019ea3cd09599e85f5
liveforone <yc4852@gmail.com>
2023-01-24 20:59:32 +0900

    Add bankbook entity.

---
+++ src/main/java/intelligent_bank/intelligent_bank/bankbook/model/BankBook.java
@@ -0,0 +1,43 @@
+package intelligent_bank.intelligent_bank.bankbook.model;
+
+import intelligent_bank.intelligent_bank.member.model.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class BankBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int bankBookNum;
+
+    private long balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDate createdDate;
+
+    @Builder
+    public BankBook(Long id, int bankBookNum, long balance, Member member) {
+        this.id = id;
+        this.bankBookNum = bankBookNum;
+        this.balance = balance;
+        this.member = member;
+    }
+}
