e55b0380473fdefb97430012ac1b4c2fdcac0cd0
liveforone <yc4852@gmail.com>
2023-01-25 14:41:09 +0900

    Add bankbook mapper class.

---
+++ src/main/java/intelligent_bank/intelligent_bank/bankbook/util/BankBookMapper.java
@@ -0,0 +1,27 @@
+package intelligent_bank.intelligent_bank.bankbook.util;
+
+import intelligent_bank.intelligent_bank.bankbook.dto.BankBookRequest;
+import intelligent_bank.intelligent_bank.bankbook.dto.BankBookResponse;
+import intelligent_bank.intelligent_bank.bankbook.model.BankBook;
+
+public class BankBookMapper {
+
+    public static BankBook dtoToEntity(BankBookRequest bankBookRequest) {
+        return BankBook.builder()
+                .id(bankBookRequest.getId())
+                .bankBookNum(bankBookRequest.getBankBookNum())
+                .balance(bankBookRequest.getBalance())
+                .member(bankBookRequest.getMember())
+                .build();
+    }
+
+    public static BankBookResponse entityToDtoDetail(BankBook bankBook) {
+        return BankBookResponse.builder()
+                .id(bankBook.getId())
+                .bankBookNum(bankBook.getBankBookNum())
+                .balance(bankBook.getBalance())
+                .member(bankBook.getMember().getRealName())
+                .createdDate(bankBook.getCreatedDate())
+                .build();
+    }
+}
