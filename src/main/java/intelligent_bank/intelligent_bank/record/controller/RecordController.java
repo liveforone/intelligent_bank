f0fe9c2b6e44cd40e88fad4293490443c141ecec
liveforone <yc4852@gmail.com>
2023-01-26 14:10:45 +0900

    Add my record api logic.

---
+++ src/main/java/intelligent_bank/intelligent_bank/record/controller/RecordController.java
@@ -0,0 +1,41 @@
+package intelligent_bank.intelligent_bank.record.controller;
+
+import intelligent_bank.intelligent_bank.member.model.Member;
+import intelligent_bank.intelligent_bank.member.service.MemberService;
+import intelligent_bank.intelligent_bank.record.dto.RecordResponse;
+import intelligent_bank.intelligent_bank.record.service.RecordService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class RecordController {
+
+    private final RecordService recordService;
+    private final MemberService memberService;
+
+    @GetMapping("/my-record")
+    public ResponseEntity<?> getMyRecord(
+            @PageableDefault(page = 0, size = 10)
+            @SortDefault.SortDefaults({
+                    @SortDefault(sort = "id", direction = Sort.Direction.DESC)
+            }) Pageable pageable,
+            Principal principal
+    ) {
+        Member member = memberService.getMemberEntity(principal.getName());
+        Page<RecordResponse> records = recordService.getRecordsByMember(member, pageable);
+
+        return ResponseEntity.ok(records);
+    }
+}
---
+++ src/main/java/intelligent_bank/intelligent_bank/record/util/RecordMapper.java
@@ -0,0 +1,31 @@
+package intelligent_bank.intelligent_bank.record.util;
+
+import intelligent_bank.intelligent_bank.record.dto.RecordRequest;
+import intelligent_bank.intelligent_bank.record.dto.RecordResponse;
+import intelligent_bank.intelligent_bank.record.model.Record;
+import org.springframework.data.domain.Page;
+
+public class RecordMapper {
+
+    public static Record dtoToEntity(RecordRequest recordRequest) {
+        return Record.builder()
+                .id(recordRequest.getId())
+                .title(recordRequest.getTitle())
+                .money(recordRequest.getMoney())
+                .bankBook(recordRequest.getBankBook())
+                .build();
+    }
+
+    private static RecordResponse dtoBuilder(Record record) {
+        return RecordResponse.builder()
+                .id(record.getId())
+                .title(record.getTitle())
+                .money(record.getMoney())
+                .createdDate(record.getCreatedDate())
+                .build();
+    }
+
+    public static Page<RecordResponse> entityToDtoPage(Page<Record> records) {
+        return records.map(RecordMapper::dtoBuilder);
+    }
+}
