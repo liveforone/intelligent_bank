9d49193969552b3bfe213e23b83e4482753016da
liveforone <yc4852@gmail.com>
2023-01-27 12:17:08 +0900

    Add calculate response.

--- src/main/java/intelligent_bank/intelligent_bank/calculate/dto/CalculateResponse.java
+++ src/main/java/intelligent_bank/intelligent_bank/calculate/dto/CalculateResponse.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CalculateResponse {
 
-    private long expense;
-    private long income;
+    private Long expense;
+    private Long income;
 }
