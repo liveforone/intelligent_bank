89ca4c7e95405b4e5d47c1334266b9be959d5f4b
liveforone <yc4852@gmail.com>
2023-02-08 14:47:21 +0900

    Add method name parsing in logger system.

--- src/main/java/intelligent_bank/intelligent_bank/aop/stopwatch/LogAspect.java
+++ src/main/java/intelligent_bank/intelligent_bank/aop/stopwatch/LogAspect.java
@@ -3,10 +3,13 @@ package intelligent_bank.intelligent_bank.aop.stopwatch;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+
+import java.lang.reflect.Method;
 
 @Component
 @Aspect
@@ -16,8 +19,11 @@ public class LogAspect {
 
     @Around("@annotation(LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+
         StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        stopWatch.start(method.getName());
 
         Object proceed = joinPoint.proceed();
 
