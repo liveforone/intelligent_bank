9731c70381944cbf9b5988dd453900482e81bf0d
liveforone <yc4852@gmail.com>
2023-02-09 17:35:22 +0900

    Add validation in atm request.

--- src/main/java/intelligent_bank/intelligent_bank/atm/dto/AtmRequest.java
+++ src/main/java/intelligent_bank/intelligent_bank/atm/dto/AtmRequest.java
@@ -1,13 +1,22 @@
 package intelligent_bank.intelligent_bank.atm.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 public class AtmRequest {
 
+    @NotNull(message = "현금을 입력하세요.")
     private long inputMoney;
+
+    @Size(min = 13, max = 13, message = "계좌번호는 13자리입니다. 정확히 입력해주세요.")
     private String bankBookNum;
+
+    @NotBlank(message = "비밀번호를 정확히 입력해주세요.")
     private String password;
 }
