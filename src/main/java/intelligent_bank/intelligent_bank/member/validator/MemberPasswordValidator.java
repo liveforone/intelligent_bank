92dce7b5c083395f428e02a27a7efac0da845692
liveforone <yc4852@gmail.com>
2023-02-09 17:07:51 +0900

    Refactor member password class change name to MemberPasswordValidator.

--- src/main/java/intelligent_bank/intelligent_bank/member/validator/MemberPasswordValidator.java
+++ src/main/java/intelligent_bank/intelligent_bank/member/validator/MemberPasswordValidator.java
@@ -1,12 +1,14 @@
-package intelligent_bank.intelligent_bank.member.util;
+package intelligent_bank.intelligent_bank.member.validator;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class MemberPassword {
+public class MemberPasswordValidator {
     static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public static boolean isNotMatchingPassword(String inputPassword, String originalPassword) {
         return !passwordEncoder.matches(inputPassword, originalPassword);
     }
+
     public static String encodePassword(String password) {
         return passwordEncoder.encode(password);
     }
